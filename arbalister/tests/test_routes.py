f4de505ad06078f2a03913b26afba7bf466a49d5
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-12-11 11:31:42 +0100

    Parametrize server limit test

--- arbalister/tests/test_routes.py
+++ arbalister/tests/test_routes.py
@@ -1,5 +1,7 @@
 import json
 import pathlib
+import random
+import string
 from typing import Awaitable, Callable
 
 import pyarrow as pa
@@ -9,19 +11,24 @@ import tornado
 import arbalister as arb
 
 
-@pytest.fixture(params=list(arb.arrow.FileFormat))
+@pytest.fixture(params=list(arb.arrow.FileFormat), scope="session")
 def file_format(request: pytest.FixtureRequest) -> arb.arrow.FileFormat:
     """Parametrize the file format used in the test."""
     out: arb.arrow.FileFormat = request.param
     return out
 
 
-@pytest.fixture
+DUMMY_TABLE_ROW_COUNT = 10
+
+
+@pytest.fixture(scope="module")
 def dummy_table() -> pa.Table:
     """Generate a table with fake data."""
     data = {
-        "letter": list("abcdefghij"),
-        "number": list(range(10)),
+        "lower": random.choices(string.ascii_lowercase, k=DUMMY_TABLE_ROW_COUNT),
+        "sequence": list(range(DUMMY_TABLE_ROW_COUNT)),
+        "upper": random.choices(string.ascii_uppercase, k=DUMMY_TABLE_ROW_COUNT),
+        "number": [random.random() for _ in range(DUMMY_TABLE_ROW_COUNT)],
     }
     return pa.table(data)
 
@@ -52,24 +59,37 @@ async def test_ipc_route(jp_fetch: JpFetch, dummy_table: pa.Table, dummy_table_f
     assert dummy_table.cast(payload.schema) == payload
 
 
-async def test_ipc_route_limit(
-    jp_fetch: JpFetch, dummy_table: pa.Table, dummy_table_file: pathlib.Path
+@pytest.mark.parametrize(
+    "params",
+    [
+        arb.routes.IpcParams(row_chunk=0, row_chunk_size=3),
+        arb.routes.IpcParams(row_chunk=1, row_chunk_size=2),
+        arb.routes.IpcParams(row_chunk=0, row_chunk_size=DUMMY_TABLE_ROW_COUNT),
+        arb.routes.IpcParams(row_chunk=1, row_chunk_size=DUMMY_TABLE_ROW_COUNT // 2 + 1),
+    ],
+)
+async def test_ipc_route_limit_row(
+    jp_fetch: JpFetch,
+    dummy_table: pa.Table,
+    dummy_table_file: pathlib.Path,
+    params: arb.routes.IpcParams,
 ) -> None:
-    """Test fetching a file returns the limited data in IPC."""
-    row_chunk_size = 2
-    row_chunk = 1
+    """Test fetching a file returns the limited rows in IPC."""
     response = await jp_fetch(
         "arrow/stream",
         str(dummy_table_file),
-        params={"row_chunk_size": row_chunk_size, "row_chunk": row_chunk},
+        params={"row_chunk_size": params.row_chunk_size, "row_chunk": params.row_chunk},
     )
 
     assert response.code == 200
     assert response.headers["Content-Type"] == "application/vnd.apache.arrow.stream"
-
     payload = pa.ipc.open_stream(response.body).read_all()
-    assert payload.num_rows == row_chunk_size
-    assert dummy_table.slice(row_chunk * row_chunk_size, row_chunk_size).cast(payload.schema) == payload
+
+    if (size := params.row_chunk_size) is not None and (cidx := params.row_chunk) is not None:
+        expected_num_rows = min((size * (cidx + 1)), dummy_table.num_rows) - (size * cidx)
+        assert payload.num_rows == expected_num_rows
+        expected = dummy_table.slice(cidx * size, size)
+        assert expected.cast(payload.schema) == payload
 
 
 async def test_stats_route(jp_fetch: JpFetch, dummy_table: pa.Table, dummy_table_file: pathlib.Path) -> None:
