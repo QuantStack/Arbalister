f97df3702ef963faff96918a9f926f04bf153bdd
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-12-30 16:19:15 +0100

    Update UI tests ports

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -7,8 +7,12 @@ module.exports = {
   ...baseConfig,
   webServer: {
     command: "jlpm start",
-    url: "http://localhost:8888/lab",
+    url: "http://localhost:53729/lab",
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    ...baseConfig.use,
+    baseURL: "http://127.0.0.1:53729",
   },
 };
